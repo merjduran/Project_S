57c3eb8bf7f7804cdb34d6efd1e2fe7f7d4da549
merjduran <merjduran@su.edu.ph>
2023-12-31 15:36:14 +0800

    add beforeEach

--- tests/e2e/place-order.spec.ts
+++ tests/e2e/place-order.spec.ts
@@ -2,14 +2,20 @@ import { test } from '@playwright/test';
 import { ProductPage } from '../../pageObjects/ProductPage';
 import { CartPage } from '../../pageObjects/CartPage';
 
+let product: ProductPage;
+let cart: CartPage
+
+test.beforeEach(async ({page}) => {
+   product = new ProductPage(page);
+   cart = new CartPage(page)
+});
+
 test('Place order for single product with complete checkout details', async ({ page }) => {
-    const product = new ProductPage(page);
     await product.navigate()
     await product.ensureProductPage()
     var total =  parseFloat(await product.selectProduct('Nokia lumia 1520'))
     await product.addProductToCart()
     await product.navigateToCartPage()
-    const cart = new CartPage(page)
     await cart.ensureProduct('Nokia lumia 1520')
     await cart.placeOrder("Name", "Country", "City", "CreditCard", "Month", "Year", total)
     await cart.ensureOrder()
@@ -17,7 +23,6 @@ test('Place order for single product with complete checkout details', async ({ p
 });
 
 test('Place order for multiple products product with complete checkout details', async ({ page }) => {
-  const product = new ProductPage(page);
   var total;
   await product.navigate()
   await product.ensureProductPage()
@@ -30,7 +35,6 @@ test('Place order for multiple products product with complete checkout details',
   await product.addProductToCart()
 
   await product.navigateToCartPage()
-  const cart = new CartPage(page)
   await cart.ensureProduct('Nokia lumia 1520')
   await cart.ensureProduct('Samsung galaxy s6')
   await cart.placeOrder("Name", "Country", "City", "CreditCard", "Month", "Year", total)
@@ -39,13 +43,11 @@ test('Place order for multiple products product with complete checkout details',
 });
 
 test('Place order with incomplete checkout details', async ({ page }) => {
-  const product = new ProductPage(page);
   await product.navigate()
   await product.ensureProductPage()
   var total =  parseFloat(await product.selectProduct('Nokia lumia 1520'))
   await product.addProductToCart()
   await product.navigateToCartPage()
-  const cart = new CartPage(page)
   await cart.ensureProduct('Nokia lumia 1520')
   await cart.placeOrder("", "", "", "", "", "", total)
   await page.close()
