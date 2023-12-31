241dbc7bf29579cb899a646315a39b622c7aa11b
merjduran <merjduran@su.edu.ph>
2023-12-31 16:15:08 +0800

    add /fixtures

--- tests/e2e/place-order.spec.ts
+++ tests/e2e/place-order.spec.ts
@@ -1,10 +1,16 @@
 import { test } from '@playwright/test';
 import { ProductPage } from '../../pageObjects/ProductPage';
 import { CartPage } from '../../pageObjects/CartPage';
+import fs from 'fs';
+import path from 'path'
+import { parse } from 'csv-parse/sync'
 
 let product: ProductPage;
 let cart: CartPage
-
+let [data] = parse(fs.readFileSync(path.join(__dirname,'../..','fixtures', 'TestData.csv')), {
+  columns: true,
+  skip_empty_lines: true
+});
 test.beforeEach(async ({page}) => {
    product = new ProductPage(page);
    cart = new CartPage(page)
@@ -13,11 +19,11 @@ test.beforeEach(async ({page}) => {
 test('Place order for single product with complete checkout details', async ({ page }) => {
     await product.navigate()
     await product.ensureProductPage()
-    var total =  parseFloat(await product.selectProduct('Nokia lumia 1520'))
+    var total =  parseFloat(await product.selectProduct(data.product_1))
     await product.addProductToCart()
     await product.navigateToCartPage()
-    await cart.ensureProduct('Nokia lumia 1520')
-    await cart.placeOrder("Name", "Country", "City", "CreditCard", "Month", "Year", total)
+    await cart.ensureProduct(data.product_1)
+    await cart.placeOrder(data.name, data.country, data.city, data.creditcard, data.month, data.year, total)
     await cart.ensureOrder()
     await page.close()
 });
@@ -26,18 +32,18 @@ test('Place order for multiple products product with complete checkout details',
   var total;
   await product.navigate()
   await product.ensureProductPage()
-  total =  parseFloat(await product.selectProduct('Nokia lumia 1520'))
+  total =  parseFloat(await product.selectProduct(data.product_1))
   await product.addProductToCart()
   //PRODUCT 2
   await product.navigateToHomePage()
   await product.ensureProductPage()
-  total =  total + parseFloat(await product.selectProduct('Samsung galaxy s6'))
+  total =  total + parseFloat(await product.selectProduct(data.product_2))
   await product.addProductToCart()
 
   await product.navigateToCartPage()
-  await cart.ensureProduct('Nokia lumia 1520')
-  await cart.ensureProduct('Samsung galaxy s6')
-  await cart.placeOrder("Name", "Country", "City", "CreditCard", "Month", "Year", total)
+  await cart.ensureProduct(data.product_1)
+  await cart.ensureProduct(data.product_2)
+  await cart.placeOrder(data.name, data.country, data.city, data.creditcard, data.month, data.year, total)
   await cart.ensureOrder()
   await page.close()
 });
@@ -45,10 +51,10 @@ test('Place order for multiple products product with complete checkout details',
 test('Place order with incomplete checkout details', async ({ page }) => {
   await product.navigate()
   await product.ensureProductPage()
-  var total =  parseFloat(await product.selectProduct('Nokia lumia 1520'))
+  var total =  parseFloat(await product.selectProduct(data.product_1))
   await product.addProductToCart()
   await product.navigateToCartPage()
-  await cart.ensureProduct('Nokia lumia 1520')
+  await cart.ensureProduct(data.product_1)
   await cart.placeOrder("", "", "", "", "", "", total)
   await page.close()
 });
